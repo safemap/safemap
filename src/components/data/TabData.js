206bb8097b0f58a4092d5ec6d066a9dc749de94b
safemap <0xsecret@gmail.com>
2016-09-13 17:26:05 +0900

    Create TabData.js

---
+++ src/components/data/TabData.js
@@ -0,0 +1,8 @@
+const TabData = () => ({
+	url: null,
+	method: ['get'],
+	dummy: [{ name : 'Tab A', key: '1', contents : 'earthquake'},
+			{ name : 'Tab B', key: '2', contents : 'earthquake'}]
+});
+
+export default TabData;
